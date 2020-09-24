72502a1de34bf0b0240f8bcc55cbfaf9ea40682b
wujixian <654751637@qq.com>
2020-09-24 21:10:42 +0800

    wxss联系以及官方样式库

---
+++ pages/wxss/wxss.js
@@ -0,0 +1,30 @@
+// pages/wxss/wxss.js
+Page({
+  mixins: [require('./mixin/themeChanged')],
+  data: {
+    inputShowed: false,
+    inputVal: ""
+},
+
+showInput: function () {
+    this.setData({
+        inputShowed: true
+    });
+},
+hideInput: function () {
+    this.setData({
+        inputVal: "",
+        inputShowed: false
+    });
+},
+clearInput: function () {
+    this.setData({
+        inputVal: ""
+    });
+},
+inputTyping: function (e) {
+    this.setData({
+        inputVal: e.detail.value
+    });
+}
+})
